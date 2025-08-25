356cfdcab177e42b17b91d67cff44c634b6dc00e
Alessandro X. Moraes <alessandromoraes267@gmail.com>
2025-08-24 23:25:13 -0300

    comm

---
+++ Studies/SequencialStructureExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace SequencialStructureExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sum
+            int a, b, sum;
+
+            Console.WriteLine("Enter a:");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter b:");
+            b = int.Parse(Console.ReadLine());
+
+            sum = a + b;
+            Console.WriteLine($"Your result is: {sum}");
+
+            // Radius of a circle
+            double radius, area;
+            
+            Console.WriteLine("Enter the radius of a circule:");
+            
+            radius = Double.Parse(Console.ReadLine());
+            area = 3.14159 * Math.Pow(radius, 2);
+            
+            Console.WriteLine($"The circle's area is: {area}", CultureInfo.InvariantCulture);
+        }
+    }
+}
