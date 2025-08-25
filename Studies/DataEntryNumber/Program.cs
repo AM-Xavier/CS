1b1bc66cac0b2f86c8c856b4d22d064ca2e1fa82
Alessandro X. Moraes <alessandromoraes267@gmail.com>
2025-08-24 21:49:06 -0300

    comm

---
+++ Studies/DataEntryNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DataEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data entry with numbers
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("You typed: " + n1);
+        }
+    }
+}
