8d1d3b6fb060a2e5d30866a493614bab533b1dbb
Alessandro X. Moraes <alessandromoraes267@gmail.com>
2025-08-28 16:54:00 -0300

    comm

--- Studies/Division/Program.cs
+++ Studies/Division/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Division
+﻿using System;
+using System.Globalization;
+using System.Runtime.ConstrainedExecution;
+
+namespace Division
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+            // segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".
+
+            Console.Write("Enter the number of divisions you want to do: ");
+            int div = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < div; i++)
+            {
+                Console.Write("Enter two numbers: ");
+                string[] numbers = Console.ReadLine().Split(' ');
+                
+                double x = double.Parse(numbers[0]),
+                    y = double.Parse(numbers[1]),
+                    result = x / y;
+
+                if (y <= 0 || x <= 0)
+                {
+                    Console.WriteLine("Impossible division.");
+                }
+
+                else
+                {
+                    Console.WriteLine($"The result is {result.ToString("F1", CultureInfo.InvariantCulture)}");
+                }
+
+            }
         }
     }
 }
