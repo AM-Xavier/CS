7abcc90428038df06e12189c89487e6ebd5abab1
Alessandro X. Moraes <alessandromoraes267@gmail.com>
2025-09-02 17:23:55 -0300

    comm

---
+++ Studies/Employee/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Employee
+{
+    internal class Employee
+    {
+        static void Main(string[] args)
+        {
+            // name: string
+            // fullsalary: double
+            // taxes: double
+            // employee info
+            
+            // liquidsalary(): double
+            // salaryincrease(percentage: double): void
+
+        }
+    }
+}
---
+++ Studies/Employee/EmployeeInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class EmployeeInfo
+    {
+        public string Name;
+        public double Salary, Taxes;
+
+        public double LiquidSalary()
+        {
+            return Salary - Taxes;
+        }
+
+        public void SalaryIncrease(double increase)
+        {
+            Salary = Salary * (increase/100);
+        }
+
+
+    }
+}
