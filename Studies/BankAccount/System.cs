9c55b79997b2e6a52b709a91c3c479d1f64e5af4
Alessandro X. Moraes <alessandromoraes267@gmail.com>
2025-10-21 16:20:16 -0300

    comm

---
+++ Studies/BankAccount/System.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace BankAccount {
+    internal class System {
+        public int AccountNumber { get; private set; }
+        public string Name { get; private set; }
+        public bool InitialDeposit { get; private set; }
+        public double IDValue { get; private set; }
+        public double Deposit { get; private set; }
+        public double Withdraw { get; private set; }
+
+        public System(int accountNumber, string name, bool initialDeposit, double iDValue) {
+            AccountNumber = accountNumber;
+            Name = name;
+            InitialDeposit = initialDeposit;
+            IDValue = iDValue;
+        }
+        public System(int accountNumber, string name, bool initialDeposit) {
+            AccountNumber = accountNumber;
+            Name = name;
+            InitialDeposit = initialDeposit;
+        }
+
+        public double DepositValue(double deposit) {
+            return Deposit += deposit;
+        }
+        public double WithdrawValue(double withdraw) {
+            return Withdraw += withdraw + 5.0;
+        }
+
+        public override string ToString() {
+            if(InitialDeposit == true) {
+                return "Account "
+                + AccountNumber
+                + ", Holder: "
+                + Name
+                + ", Balance: $ "
+                + (IDValue + Deposit - Withdraw).ToString("F2", CultureInfo.InvariantCulture);
+            }
+            else {
+                return "Account "
+                + AccountNumber
+                + ", Holder: "
+                + Name
+                + ", Balance: $ "
+                + (Deposit - Withdraw).ToString("F2", CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
